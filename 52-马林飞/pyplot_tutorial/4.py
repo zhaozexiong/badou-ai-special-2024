11ba688100a44c5006d706c2ad94be833693212c
malf <cutemalinfei@outlook.com>
2024-04-11 10:18:04 +0800

    del-删除一些不是主分支的文件

--- 52-马林飞/pyplot_tutorial/4.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.arange(0, 10)
-sin_line = plt.plot(x, np.sin(x), linewidth=3.0)  # 使用 sin_line, = plt.plot(x, np.sin(x) 等同于 sin_line[0]
-sin_line[0].set_antialiased(False)
-plt.gca().add_line(sin_line[0])
-plt.show()
--- 52-马林飞/pyplot_tutorial/5.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.arange(0., 10.)
-
-lines = plt.plot(x, x ** 2, 'r-', x / 2, x * 3, 'g*')
-plt.setp(lines[0], linewidth=3.0, color='y')  # 用来设置线段的属性
-
-plt.show()
