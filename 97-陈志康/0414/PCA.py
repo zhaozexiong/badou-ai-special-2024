587825bc6048a52d99c9d09c03a79b8c7ba686a3
czk <314222151@qq.com>
2024-04-20 17:47:00 +0800

    1.椒盐、高斯噪声 2.PCA实现

--- 97-陈志康/0414/PCA.py
+++ 97-陈志康/0414/PCA.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.decomposition import PCA
 
 X = np.array(
-    [[-1, 2, 66, -1], [-2, 6, 58, -1], [-3, 8, 45, -2], [1, 9, 36, 1], [2, 10, 62, 1], [3, 5, 83, 2]])  # 导入数据，维度为4
+    [[-2, 23, 86, -11], [-9, 5, 98, 1], [3, 18, 23, 1], [-1, 19, 26, 10], [12, 100, 162, 3], [13, 45, 283, 29]])  # 导入数据，维度为4
 pca = PCA(n_components=2)  # 降到2维
 pca.fit(X)  # 执行
 newX = pca.fit_transform(X)  # 降维后的数据
