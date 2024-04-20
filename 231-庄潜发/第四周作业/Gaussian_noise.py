7b92586a6ecc52dc62c3b5c5833bf576c485c34e
Zhuang-qf <121532292+Zhuang-qf@users.noreply.github.com>
2024-04-20 18:26:51 +0800

    Add files via upload

---
+++ 231-庄潜发/第四周作业/Gaussian_noise.py
@@ -0,0 +1,39 @@
+"""
+@Author: zhuang_qf
+@encoding: utf-8
+@time: 2024/4/20 10:22
+"""
+import random
+import cv2
+
+
+def get_gauss_img(percentage, means, sigma):
+    # 生成存放噪声图像
+    noise_img = gray_img
+    # 计算图片总像素点
+    pixel_sum = gray_img.shape[0]*gray_img.shape[1]
+    # 计算你想要百分比的高斯噪声的像素点
+    percents = int(pixel_sum * percentage)
+    for i in range(percents):
+        # 随机取原图中的行X和列Y, 边缘不处理
+        randX = random.randint(0, gray_img.shape[0]-1)
+        randY = random.randint(0, gray_img.shape[1]-1)
+        # 计算高斯像素值并赋值
+        noise_img[randX, randY] = noise_img[randX, randY] + random.gauss(means, sigma)
+        # 控制像素点的范围在0-255
+        if noise_img[randX, randY] > 255:
+            noise_img[randX, randY] = 255
+        if noise_img[randX, randY] < 0:
+            noise_img[randX, randY] = 0
+    return noise_img
+
+
+if __name__ == '__main__':
+    # 参数0,表示图片读取为灰度图
+    gray_img = cv2.imread("../lenna.png", 0)
+    cv2.imshow("gray_img", gray_img)
+    # 参数1:百分比噪声, 均值means, 标准差sigma
+    gauss_img = get_gauss_img(0.8, 2, 4)
+    cv2.imshow("gauss_img", gauss_img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
