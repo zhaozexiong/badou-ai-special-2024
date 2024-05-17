ee2f4a10300d18659bba2fdbcaf60bbb3611a8ea
fgxxxxx <145818881+fgxxxxx@users.noreply.github.com>
2024-05-18 01:46:29 +0800

    Add files via upload

---
+++ 244-方光贤-福州/week6/Kmenas_athletes.py
@@ -0,0 +1,82 @@
+# coding=utf-8
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+"""
+第一部分：数据集
+X表示二维矩阵数据，篮球运动员比赛数据
+总共20行，每行两列数据
+第一列表示球员每分钟助攻数：assists_per_minute
+第二列表示球员每分钟得分数：points_per_minute
+"""
+X = [[0.0888, 0.5885],
+     [0.1399, 0.8291],
+     [0.0747, 0.4974],
+     [0.0983, 0.5772],
+     [0.1276, 0.5703],
+     [0.1671, 0.5835],
+     [0.1306, 0.5276],
+     [0.1061, 0.5523],
+     [0.2446, 0.4007],
+     [0.1670, 0.4770],
+     [0.2485, 0.4313],
+     [0.1227, 0.4909],
+     [0.1240, 0.5668],
+     [0.1461, 0.5113],
+     [0.2315, 0.3788],
+     [0.0494, 0.5590],
+     [0.1107, 0.4799],
+     [0.1121, 0.5735],
+     [0.1007, 0.6318],
+     [0.2567, 0.4326],
+     [0.1956, 0.4280]
+     ]
+
+# 输出数据集
+print(X)
+
+"""
+第二部分：KMeans聚类
+clf = KMeans(n_clusters=3) 表示类簇数为3，聚成3类数据，clf即赋值为KMeans
+y_pred = clf.fit_predict(X) 载入数据集X，并且将聚类的结果赋值给y_pred
+"""
+
+clf = KMeans(n_clusters=3)
+y_pred = clf.fit_predict(X)
+
+# 输出完整Kmeans函数
+print(clf)
+# 输出聚类预测结果
+print("y_pred = ", y_pred)
+
+"""
+第三部分：可视化绘图
+"""
+
+# 获取数据集的第一列和第二列数据 使用for循环获取 n[0]表示X第一列 n[1]表示X第二列
+x = [n[0] for n in X]
+print(x)
+y = [n[1] for n in X]
+print(y)
+
+''' 
+绘制散点图 
+参数：x横轴; y纵轴; c=y_pred聚类预测结果; marker类型:o表示圆点,*表示星型,x表示点;
+'''
+# 绘制散点图
+plt.scatter(x, y, c=y_pred, marker='x')
+
+# 为每个聚类创建一个代表点
+colors = ['red', 'green', 'blue']  # 假设有3个聚类，可以定义不同的颜色
+legend_handles = []
+for i in range(3):
+     scatter = plt.scatter([], [], c=colors[i], marker='x', label=f'Cluster {i + 1}', s=50)  # s参数设置点的大小
+     legend_handles.append(scatter)
+
+# 绘制标题 坐标轴和图例
+plt.title("Kmeans-Basketball Data")
+plt.xlabel("assists_per_minute")
+plt.ylabel("points_per_minute")
+plt.legend(handles=legend_handles, loc='upper right')  # 使用之前创建的句柄
+
+# 显示图形
+plt.show()
