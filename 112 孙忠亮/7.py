ab31f274eca82e906d4eedaa96569c10c4b42c2f
yszllu <164169693+yszllu@users.noreply.github.com>
2024-05-23 21:59:51 +0800

    Add files via upload

---
+++ 112 孙忠亮/7.py
@@ -0,0 +1,87 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.cluster import DBSCAN
+import cv2
+#密度聚类
+iris = datasets.load_iris()
+X = iris.data[:, :4]
+dbscan = DBSCAN(eps=0.4, min_samples=9)
+dbscan.fit(X)
+label_pred = dbscan.labels_
+
+# 绘制结果
+x0 = X[label_pred == 0]
+x1 = X[label_pred == 1]
+x2 = X[label_pred == 2]
+plt.scatter(x0[:, 0], x0[:, 1], c="red", marker='o', label='label0')
+plt.scatter(x1[:, 0], x1[:, 1], c="green", marker='*', label='label1')
+plt.scatter(x2[:, 0], x2[:, 1], c="blue", marker='+', label='label2')
+plt.xlabel('sepal length')
+plt.ylabel('sepal width')
+plt.legend(loc=2)
+plt.show()
+from scipy.cluster.hierarchy import dendrogram, linkage,fcluster
+from matplotlib import pyplot as plt
+#层次聚类
+X = [[1,2],[3,2],[4,4],[1,2],[1,3]]
+Z = linkage(X, 'ward')
+f = fcluster(Z,4,'distance')
+fig = plt.figure(figsize=(5, 3))
+dn = dendrogram(Z)
+print(Z)
+plt.show()
+
+#sift
+def drawMatchesKnn_cv2(img1_gray, kp1, img2_gray, kp2, goodMatch):
+    h1, w1 = img1_gray.shape[:2]
+    h2, w2 = img2_gray.shape[:2]
+
+    vis = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
+    vis[:h1, :w1] = img1_gray
+    vis[:h2, w1:w1 + w2] = img2_gray
+
+    p1 = [kpp.queryIdx for kpp in goodMatch]
+    p2 = [kpp.trainIdx for kpp in goodMatch]
+
+    post1 = np.int32([kp1[pp].pt for pp in p1])
+    post2 = np.int32([kp2[pp].pt for pp in p2]) + (w1, 0)
+
+    for (x1, y1), (x2, y2) in zip(post1, post2):
+        cv2.line(vis, (x1, y1), (x2, y2), (0, 0, 255))
+
+    cv2.namedWindow("match", cv2.WINDOW_NORMAL)
+    cv2.imshow("match", vis)
+
+
+img1_gray = cv2.imread("iphone1.png")
+img2_gray = cv2.imread("iphone2.png")
+
+sift = cv2.xfeatures2d.SIFT_create()
+kp1, des1 = sift.detectAndCompute(img1_gray, None)
+kp2, des2 = sift.detectAndCompute(img2_gray, None)
+
+bf = cv2.BFMatcher(cv2.NORM_L2)
+matches = bf.knnMatch(des1, des2, k=2)
+goodMatch = []
+for m, n in matches:
+    if m.distance < 0.50 * n.distance:
+        goodMatch.append(m)
+
+drawMatchesKnn_cv2(img1_gray, kp1, img2_gray, kp2, goodMatch[:20])
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+img = cv2.imread("lenna.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+sift = cv2.xfeatures2d.SIFT_create()
+keypoints, descriptor = sift.detectAndCompute(gray, None)
+
+img = cv2.drawKeypoints(image=img, outImage=img, keypoints=keypoints,
+                        flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS,
+                        color=(51, 163, 236))
+cv2.imshow('sift_keypoints', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
